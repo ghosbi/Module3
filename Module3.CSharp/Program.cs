71cc8e30d68fe989f66ff4da800136a7ec6046ad
Pavel Konstantinov <Pavel.romon@gmail.com>
2024-07-22 18:59:09 +0500

    Добавили #

--- Module3.CSharp/Program.cs
+++ Module3.CSharp/Program.cs
@@ -8,6 +8,12 @@
             myName = "Pavel";
 
             Console.WriteLine(myName);
+
+            Console.WriteLine("\t Привет Мир");
+            Console.WriteLine("\t Мне 26 лет");
+            Console.WriteLine("\t Моя имя \n {0}", myName);
+            Console.WriteLine("\u0040");
+            Console.WriteLine("\x23");
         }
     }
 }
