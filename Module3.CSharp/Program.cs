12fc576fc2a4cbbccfb91cf720113dd43f1b7653
Pavel Konstantinov <Pavel.romon@gmail.com>
2024-07-22 19:02:33 +0500

    Выполнили задание 3.3.2

--- Module3.CSharp/Program.cs
+++ Module3.CSharp/Program.cs
@@ -6,14 +6,23 @@
         {
             string myName;
             myName = "Pavel";
+            byte myAge = 26;
+            bool haveIpet = false;
+            double myShoeSize = 42.5;
+
 
             Console.WriteLine(myName);
 
             Console.WriteLine("\t Привет Мир");
-            Console.WriteLine("\t Мне 26 лет");
+            Console.WriteLine("\t Мне {0}", myAge + " лет");
             Console.WriteLine("\t Моя имя \n {0}", myName);
             Console.WriteLine("\u0040");
             Console.WriteLine("\x23");
+            Console.WriteLine("Есть ли у меня питомец? " +  haveIpet);
+            Console.WriteLine("Мой размер обуви: " +  myShoeSize);
+
+
+
         }
     }
 }
