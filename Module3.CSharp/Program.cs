09a70a486f95f16344e1c2f12401a1a48403e37e
Pavel Konstantinov <Pavel.romon@gmail.com>
2024-07-22 18:34:24 +0500

    Добавьте файлы проекта.

---
+++ Module3.CSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module3.CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
